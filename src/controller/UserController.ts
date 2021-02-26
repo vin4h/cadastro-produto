eb248b3bca614408bf9b3dbc3e3e1f3f8471f4d3
Vin4h <viniciu.rezende@gmail.com>
2021-02-26 13:27:19 -0400

    Usando partil, refatorando interfaces

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -3,19 +3,15 @@ import CreateUserService from '../services/user/CreateUserService';
 import UpdateUserService from '../services/user/UpdateUserServices';
 
 interface RequestCreate {
+    id: string,
     name: string,
     email: string,
     password: string
 }
 
-interface RequestUpdate {
-    id: string,
-    name?: string,
-    password?: string
-}
 
 class UserController {
-    public async create({ name, email, password }: RequestCreate): Promise<User> {
+    public async create({ name, email, password }: Partial<RequestCreate>): Promise<User> {
         const createUserService = new CreateUserService();
 
         const user = await createUserService.execute({
@@ -30,7 +26,7 @@ class UserController {
 
     }
 
-    public async update({ id, name, password }: RequestUpdate): Promise<User> {
+    public async update({ id, name, password }: Partial<RequestCreate>): Promise<User> {
         const updateUserService = new UpdateUserService();
 
         const updatedUser = await updateUserService.execute({
